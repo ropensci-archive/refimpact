a0a42d229845e391aa2c0ab619feb2dc02a69b0c
Perry Stephenson <perry.stephenson@gmail.com>
2017-01-29 23:10:55 +1100

    Changed get_institutions() to ref_institutions() to be more aligned with ROpenSci style (#17). Using the httr package with a bit more error checking (#5). Changed dontrun to donttest (#13).
    There will likely be a fair bit of duplicate code as this goes on, so it will likely be moved to a helper function (#9).

--- R/get_institutions.R
+++ R/get_institutions.R
@@ -4,20 +4,39 @@
 #' and returns a list of institutions and associated metadata, including the
 #' UKPRN.
 #'
-#' @return Returns a data_frame (from the \code{tibble} package).
+#' @return Returns a tibble (from the \code{tibble} package).
 #'
 #' @examples
-#' \dontrun{
-#' get_institutions()
+#' \donttest{
+#' ref_institutions()
 #' }
 #'
 #' @export
-get_institutions <- function() {
+ref_institutions <- function() {
 
-  ##### Call the API #####
-  query_url <- "http://impact.ref.ac.uk/casestudiesapi/REFAPI.svc/ListInstitutions"
-  tmp <- tibble::as_data_frame(jsonlite::fromJSON(query_url))
+  ua <- httr::user_agent("http://github.com/perrystephenson/refimpact")
 
-  return(tmp)
+  r <- httr::GET(
+    "http://impact.ref.ac.uk/casestudiesapi/REFAPI.svc/ListInstitutions", ua)
+
+  if (httr::http_type(r) != "application/json") {
+    stop("API did not return json", call. = FALSE)
+  }
+
+  parsed <- jsonlite::fromJSON(httr::content(r, as = "text"))
+
+  if (httr::http_error(r)) {
+    stop(
+      sprintf(
+        "API request failed [%s]\n%s\n<%s>",
+        httr::status_code(r),
+        parsed$message,
+        parsed$documentation_url
+      ),
+      call. = FALSE
+    )
+  }
+
+  tibble::as_tibble(parsed)
 
 }
